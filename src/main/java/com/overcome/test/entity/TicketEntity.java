ab191c3fe17462988e9216f619a0d43e5b358e6c
Jovany Vergara <jovany@makingdevs.com>
2023-07-21 17:05:15 -0600

    [solutionTestDiego] add model to tickets

--- src/main/java/com/overcome/test/entity/TicketEntity.java
+++ src/main/java/com/overcome/test/entity/TicketEntity.java
@@ -4,42 +4,52 @@ import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class TicketEntity {
-
-	public TicketEntity() {
-  }
 
   public TicketEntity(String title) {
     this.title = title;
   }
 
-  public TicketEntity(String title, String date, String responsableName, String responsibleTeam, String incidentType,
-    String incidenceSeverity, String problemDescription) {
+  public TicketEntity(String title, String responsableName, String responsibleTeam, String incidentType,
+    String incidenceSeverity, String versionSoftware, String problemDescription, String uploadFile) {
     this.title = title;
-    this.date = date;
+    this.createDate = new Date();
     this.responsableName = responsableName;
     this.responsibleTeam = responsibleTeam;
     this.incidentType = incidentType;
     this.incidenceSeverity = incidenceSeverity;
+    this.versionSoftware = versionSoftware;
     this.problemDescription = problemDescription;
+    this.uploadFile = uploadFile;
   }
+
   @Id Long id;
   String title;
-  String date;
+  Date createDate;
   String responsableName;
   String responsibleTeam;
   String incidentType;
   String incidenceSeverity;
+  String versionSoftware;
   String problemDescription;
+  String uploadFile;
 
   @Override
   public String toString() {
-    return "TicketEntity [id=" + id + ", title=" + title + ", date=" + date + ", responsableName=" + responsableName
+    String pattern = "yyyy-MM-dd";
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+
+    return "TicketEntity [id=" + id + ", title=" + title + ", date=" + simpleDateFormat.format(createDate) + ", responsableName=" + responsableName
         + ", responsibleTeam=" + responsibleTeam + ", incidentType=" + incidentType + ", incidenceSeverity="
-        + incidenceSeverity + ", problemDescription=" + problemDescription + "]";
+        + incidenceSeverity + ", versionSoftware=" + versionSoftware + ", problemDescription=" + problemDescription + " uploadFile=" + uploadFile + "]";
   }
 }
