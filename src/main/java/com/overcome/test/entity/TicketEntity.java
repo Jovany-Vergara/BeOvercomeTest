e3e4e86e85cc74568861e3f049f7b594b5dee8b7
Jovany Vergara <jovany@makingdevs.com>
2023-07-21 13:30:08 -0600

    [solutionTestDiego] add lombok

--- src/main/java/com/overcome/test/entity/TicketEntity.java
+++ src/main/java/com/overcome/test/entity/TicketEntity.java
@@ -2,8 +2,12 @@ package com.overcome.test.entity;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class TicketEntity {
 
 	public TicketEntity() {
@@ -31,69 +35,6 @@ public class TicketEntity {
   String incidentType;
   String incidenceSeverity;
   String problemDescription;
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getDate() {
-    return date;
-  }
-
-  public void setDate(String date) {
-    this.date = date;
-  }
-
-  public String getResponsableName() {
-    return responsableName;
-  }
-
-  public void setResponsableName(String responsableName) {
-    this.responsableName = responsableName;
-  }
-
-  public String getResponsibleTeam() {
-    return responsibleTeam;
-  }
-
-  public void setResponsibleTeam(String responsibleTeam) {
-    this.responsibleTeam = responsibleTeam;
-  }
-
-  public String getIncidentType() {
-    return incidentType;
-  }
-
-  public void setIncidentType(String incidentType) {
-    this.incidentType = incidentType;
-  }
-
-  public String getIncidenceSeverity() {
-    return incidenceSeverity;
-  }
-
-  public void setIncidenceSeverity(String incidenceSeverity) {
-    this.incidenceSeverity = incidenceSeverity;
-  }
-
-  public String getProblemDescription() {
-    return problemDescription;
-  }
-
-  public void setProblemDescription(String problemDescription) {
-    this.problemDescription = problemDescription;
-  }
 
   @Override
   public String toString() {
