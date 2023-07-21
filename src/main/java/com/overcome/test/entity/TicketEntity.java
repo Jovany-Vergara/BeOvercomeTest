bfb8eef2a18d2c0869407c0fd827b688b81f6fcd
Jovany Vergara <jovany@makingdevs.com>
2023-07-21 13:27:03 -0600

    [solutionTestDiego] add setter and getters

--- src/main/java/com/overcome/test/entity/TicketEntity.java
+++ src/main/java/com/overcome/test/entity/TicketEntity.java
@@ -1,16 +1,104 @@
 package com.overcome.test.entity;
 
-import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
 
 @Entity
 public class TicketEntity {
-  @Id Long id;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public Long getId() {
-		return id;
-	}
+	public TicketEntity() {
+  }
+
+  public TicketEntity(String title) {
+    this.title = title;
+  }
+
+  public TicketEntity(String title, String date, String responsableName, String responsibleTeam, String incidentType,
+    String incidenceSeverity, String problemDescription) {
+    this.title = title;
+    this.date = date;
+    this.responsableName = responsableName;
+    this.responsibleTeam = responsibleTeam;
+    this.incidentType = incidentType;
+    this.incidenceSeverity = incidenceSeverity;
+    this.problemDescription = problemDescription;
+  }
+  @Id Long id;
+  String title;
+  String date;
+  String responsableName;
+  String responsibleTeam;
+  String incidentType;
+  String incidenceSeverity;
+  String problemDescription;
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDate() {
+    return date;
+  }
+
+  public void setDate(String date) {
+    this.date = date;
+  }
+
+  public String getResponsableName() {
+    return responsableName;
+  }
+
+  public void setResponsableName(String responsableName) {
+    this.responsableName = responsableName;
+  }
+
+  public String getResponsibleTeam() {
+    return responsibleTeam;
+  }
+
+  public void setResponsibleTeam(String responsibleTeam) {
+    this.responsibleTeam = responsibleTeam;
+  }
+
+  public String getIncidentType() {
+    return incidentType;
+  }
+
+  public void setIncidentType(String incidentType) {
+    this.incidentType = incidentType;
+  }
+
+  public String getIncidenceSeverity() {
+    return incidenceSeverity;
+  }
+
+  public void setIncidenceSeverity(String incidenceSeverity) {
+    this.incidenceSeverity = incidenceSeverity;
+  }
+
+  public String getProblemDescription() {
+    return problemDescription;
+  }
+
+  public void setProblemDescription(String problemDescription) {
+    this.problemDescription = problemDescription;
+  }
+
+  @Override
+  public String toString() {
+    return "TicketEntity [id=" + id + ", title=" + title + ", date=" + date + ", responsableName=" + responsableName
+        + ", responsibleTeam=" + responsibleTeam + ", incidentType=" + incidentType + ", incidenceSeverity="
+        + incidenceSeverity + ", problemDescription=" + problemDescription + "]";
+  }
 }
