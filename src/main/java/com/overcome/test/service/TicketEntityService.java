f8916f6ea91b9fd98bc67ed66997c3df10969ddf
Jovany Vergara <jovany@makingdevs.com>
2023-07-21 22:52:23 -0600

    [solutionTestDiego] create service entity ticket

---
+++ src/main/java/com/overcome/test/service/TicketEntityService.java
@@ -0,0 +1,8 @@
+package com.overcome.test.service;
+
+import com.overcome.service.EntityService;
+import com.overcome.test.entity.TicketEntity;
+
+public interface TicketEntityService extends EntityService<TicketEntity> {
+
+}
